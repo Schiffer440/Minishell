e6549f73e5da9e28d4e724c8ca6758235f1deffc
mbruyant <mbruyant@student.42.fr>
2023-12-16 20:50:01 +0100

    env mod

--- env/env_init.c
+++ env/env_init.c
@@ -6,7 +6,7 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 18:41:40 by mbruyant          #+#    #+#             */
-/*   Updated: 2023/12/16 19:07:45 by mbruyant         ###   ########.fr       */
+/*   Updated: 2023/12/16 20:49:41 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,8 @@ void	ft_init_env(char **envp, t_env *env)
 		ft_putendl_fd("ft_init_env fail", 2);
 		return ;
 	}
+}
+void	ft_init_no_env(t_env *env)
+{
+	//si no env ici, est-ce qu'une liste chainee ne serait pas mieux justeeeeee
 }
